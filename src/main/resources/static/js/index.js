e0e1b473a08fbf1d7e8f012d5a6c9b61f27f97a9
joey <joey_qpb@hotmail.com>
2024-06-17 16:57:20 -0300

    feat: UI

---
+++ src/main/resources/static/js/index.js
@@ -0,0 +1,326 @@
+    function displayResult(title, result) {
+        document.getElementById('resultTitle').innerText = title;
+        document.getElementById('resultBody').innerText = JSON.stringify(result, null, 2);
+    }
+
+    const apiUrl = "localhost:8080";
+    const bookUrl = apiUrl+"/api/book";
+    const authorUrl = apiUrl+"/api/author";
+    const publisherUrl = apiUrl+"/api/publisher";
+
+    // BOOK
+
+    const createBook = async () => {
+        const book = {
+            title: document.getElementById('bookTitle').value.trim(),
+            publisherName: document.getElementById('publisherName').value.trim(),
+            authorNames: document.getElementById('authorNames').value.split(',').map(el => el.trim()),
+            reviewComment: document.getElementById('reviewComment').value
+        };
+
+        try {
+            const response = await fetch(bookUrl, {
+                method: "POST",
+                headers: { "Content-Type": "application/json" },
+                body: JSON.stringify(book)
+            });
+
+            const data = await response.json();
+            displayResult('Book - Create', data);
+        } catch (error) {
+            displayResult('Book - Create', { error: error.message });
+        }
+    }
+
+    const findAllBooks = async () => {
+        try {
+            const response = await fetch(bookUrl, {
+                method: "GET",
+                headers: { "Content-Type": "application/json" }
+            });
+
+            const result = await response.json();
+            displayResult('Book - Find All', result);
+        } catch (error) {
+            displayResult('Book - Find All', { error: error.message });
+        }
+    }
+
+    const findAllBooksPagination = async () => {
+        try {
+            const pageAndSize = document.getElementById('findAllBooksPaginationButton').value.split(',');
+            const page = pageAndSize[0];
+            const size = pageAndSize[1];
+
+            const response = await fetch(bookUrl+"?page="+page+"&size="+size, {
+                method: "GET",
+                headers: { "Content-Type": "application/json" }
+            });
+
+            const result = await response.json();
+            displayResult('Book - Find All Pagination page: ' + page + 'size: ' + size, result);
+        } catch (error) {
+            displayResult('Book - Find All Pagination', { error: error.message });
+        }
+    }
+
+    const findAllBooksByAuthorName = async () => {
+        const authorName = document.getElementById('findAllBooksByAuthorNameButton').value.trim();
+
+        try {
+            const response = await fetch(bookUrl+"/q_author="+authorName, {
+                method: "GET",
+                headers: { "Content-Type": "application/json" }
+            });
+
+            const result = await response.json();
+            displayResult('Book - Find all by author name: ' + authorName, result);
+        } catch (error) {
+            displayResult('Book - Find all by author name: ' + authorName, { error: error.message });
+        }
+    }
+
+    const findAllBooksByPublisherName = async () => {
+        const publisherName = document.getElementById('findAllBooksByPublisherNameButton').value;
+
+        try {
+            const response = await fetch(bookUrl+"/q_publisher="+publisherName, {
+                method: "GET",
+                headers: { "Content-Type": "application/json" }
+            });
+
+            const result = await response.json();
+            displayResult('Book - Find all by publisher: ' + publisherName, result);
+        } catch (error) {
+            displayResult('Book - Find all by publisher: ' + publisherName, { error: error.message });
+        }
+    }
+
+    const findBookByName = async () => {
+        const bookName = document.getElementById('findBookByNameButton').value;
+
+        try {
+            const response = await fetch(bookUrl+"/q_name="+publisherName, {
+                method: "GET",
+                headers: { "Content-Type": "application/json" }
+            });
+
+            const result = await response.json();
+            displayResult('Book - Find by Book name: ' + bookName, result);
+        } catch (error) {
+            displayResult('Book - Find by Book name: ' + bookName, { error: error.message });
+        }
+    }
+
+    const findBookById = async () => {
+        const id = document.getElementById('findBookByIdButton').value;
+
+        try {
+            const response = await fetch(bookUrl+"/"+id, {
+                method: "GET",
+                headers: { "Content-Type": "application/json" }
+            });
+
+            const result = await response.json();
+            displayResult('Book - Find book by id: ' + id, result);
+        } catch (error) {
+            displayResult('Book - Find book by id: ' + id, { error: error.message });
+        }
+    }
+
+    const deleteBook = async () => {
+        const id = document.getElementById('deleteBookId').value;
+
+        try {
+            const response = await fetch(bookUrl+"/"+id, {
+                method: "DELETE",
+                headers: { "Content-Type": "application/json" }
+            });
+
+            const result = await response.json();
+            displayResult('Book - Delete', { id, status: 'Deleted: ' + result });
+        } catch (error) {
+            displayResult('Book - Delete: ' + id, { error: error.message });
+        }
+    }
+
+    // AUTHOR
+
+    const createAuthor = async () => {
+        const author = { name: document.getElementById('authorName').value };
+
+        try {
+            const response = await fetch(authorUrl, {
+                method: "POST",
+                headers: { "Content-Type": "application/json" },
+                body: JSON.stringify(author)
+            });
+
+            const data = await response.json();
+            displayResult('Author - Create', data);
+        } catch (error) {
+            displayResult('Author - Create', { error: error.message });
+        }
+    }
+
+    const findAllAuthors = async () => {
+
+        try {
+            const response = await fetch(authorUrl, {
+                method: "GET",
+                headers: { "Content-Type": "application/json" }
+            });
+
+            const result = await response.json();
+            displayResult('Author - Find All', result);
+        } catch (error) {
+            displayResult('Author - Find All', { error: error.message });
+        }
+    }
+
+    const findAuthorByName = async () => {
+        const name = document.getElementById('findAuthorByNameButton').value;
+
+        try {
+            const response = await fetch(authorUrl+"/q_name="+name, {
+                method: "GET",
+                headers: { "Content-Type": "application/json" }
+            });
+
+            const result = await response.json();
+            displayResult('Author - Find By name: ' + name, result);
+        } catch (error) {
+            displayResult('Author - Find By name: ' + name, { error: error.message });
+        }
+    }
+
+    const findAuthorById = async () => {
+        const id = document.getElementById('findAuthorByIdButton').value;
+
+        try {
+            const response = await fetch(authorUrl+"/"+id, {
+                method: "GET",
+                headers: { "Content-Type": "application/json" }
+            });
+
+            const result = await response.json();
+            displayResult('Author - Find By Id: ' + id, result);
+        } catch (error) {
+            displayResult('Author - Find By Id: ' + id, { error: error.message });
+        }
+    }
+
+    const addBookToAuthor = async () => {
+        const ids = document.getElementById('addBookToAuthorButton').value.split(',');
+        const idAuthor = ids[0];
+        const idBook = ids[1];
+
+        try {
+            const response = await fetch(authorUrl+"/"+idAuthor+"/"+idBook, {
+                method: "PUT",
+                headers: { "Content-Type": "application/json" }
+            });
+
+            const result = await response.json();
+            displayResult('Author updated: ' + idAuthor, result);
+        } catch (error) {
+            displayResult('Author updated: ' + idAuthor, { error: error.message });
+        }
+    }
+
+    const deleteAuthor = async () => {
+        const id = document.getElementById('deleteAuthorId').value;
+
+        try {
+            const response = await fetch(authorUrl+"/"+id, {
+                method: "DELETE",
+                headers: { "Content-Type": "application/json" }
+            });
+
+            const result = await response.json();
+            displayResult('Author - Delete', { id, status: 'Deleted: ' + result });
+        } catch (error) {
+            displayResult('Author - Delete: ' + id, { error: error.message });
+        }
+    }
+
+    // PUBLISHER
+
+    const createPublisher = async () => {
+        const publisher = { name: document.getElementById('publisherNameForm').value };
+
+        try {
+            const response = await fetch(publisherUrl, {
+                method: "POST",
+                headers: { "Content-Type": "application/json" },
+                body: JSON.stringify(publisher)
+            });
+
+            const data = await response.json();
+            displayResult('Publisher - Create', data);
+        } catch (error) {
+            displayResult('Publisher - Create', { error: error.message });
+        }
+    }
+
+    const findAllPublishers = async () => {
+        try {
+            const response = await fetch(publisherUrl, {
+                method: "GET",
+                headers: { "Content-Type": "application/json" }
+            });
+
+            const result = await response.json();
+            displayResult('publisherUrl - Find All', result);
+        } catch (error) {
+            displayResult('publisherUrl - Find All', { error: error.message });
+        }
+    }
+
+    const findPublisherByName = async () => {
+        const name = document.getElementById('findPublisherByNameButton').value;
+
+        try {
+            const response = await fetch(publisherUrl+"/q_name="+name, {
+                method: "GET",
+                headers: { "Content-Type": "application/json" }
+            });
+
+            const result = await response.json();
+            displayResult('Publisher - Find By Name: ' + name, result);
+        } catch (error) {
+            displayResult('Publisher - Find By Name: ' + name, { error: error.message });
+        }
+    }
+
+    const findPublisherById = async () => {
+        const id = document.getElementById('findPublisherByIdButton').value;
+
+        try {
+            const response = await fetch(publisherUrl+"/"+id, {
+                method: "GET",
+                headers: { "Content-Type": "application/json" }
+            });
+
+            const result = await response.json();
+            displayResult('Publisher - Find By ID: ' + id, result);
+        } catch (error) {
+            displayResult('Publisher - Find By ID: ' + id, { error: error.message });
+        }
+    }
+
+    const deletePublisher = async () => {
+        const id = document.getElementById('deletePublisherId').value;
+
+        try {
+            const response = await fetch(publisherUrl+"/"+id, {
+                method: "DELETE",
+                headers: { "Content-Type": "application/json" }
+            });
+
+            const result = await response.json();
+            displayResult('Publisher - Delete', { id, status: 'Deleted: ' + result });
+        } catch (error) {
+            displayResult('Publisher - Delete', { error: error.message });
+        }
+    }

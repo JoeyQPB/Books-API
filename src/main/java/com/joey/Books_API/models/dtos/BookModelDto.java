8177026c3676bb0bd79845c5612581ba8db10661
joey <joey_qpb@hotmail.com>
2024-06-19 12:39:48 -0300

    fix: annotation

--- src/main/java/com/joey/Books_API/models/dtos/BookModelDto.java
+++ src/main/java/com/joey/Books_API/models/dtos/BookModelDto.java
@@ -1,11 +1,12 @@
 package com.joey.Books_API.models.dtos;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
 
 import java.util.Set;
 
 public record BookModelDto (@NotBlank String title,
                             @NotBlank String publisherName,
-                            @NotBlank Set<String> authorNames,
+                            @NotEmpty Set<String> authorNames,
                             String reviewComment)  {
 }
